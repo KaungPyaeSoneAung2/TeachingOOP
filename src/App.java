5e16623beb8e8b978d8d59e6665c59384b130907
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2024-11-01 17:53:53 +0630

    Init commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // What is OOP?
+        // OOP is building the codes how you build things in real world
+        // It is more focused on Objects rather than Functions.
+        // Major core feature is of OOP is reusablity of codes and Maintainability.
+        // Encapsulation, Inheritance, Polymorphism, Abstraction
+    }
+}
